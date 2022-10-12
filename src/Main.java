3b33e1f64c6b51fb083c6fb1280a7f1b4cd38bfa
mozyakov <s.mozyakov@gmail.com>
2022-10-12 12:15:33 +0700

    add change in file main.java and rename branch

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         SalesManager sales1 = new SalesManager(new int[]{10, 20, 30, 15, 11});
 
         System.out.println("Максимальная сумма одной сделки составила: " + sales1.max() + " млн $");
+
 
     }
 }
